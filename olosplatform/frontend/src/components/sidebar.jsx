2c8dc954bef2c113e85266d604de00525cbeaec9
Leandro Alves de Souza <leandro.souza@olos.com.br>
2025-07-08 15:28:33 -0300

    Adicionado os icones de navegacao

--- olosplatform/frontend/src/components/sidebar.jsx
+++ olosplatform/frontend/src/components/sidebar.jsx
@@ -3,39 +3,47 @@
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { IoMdExit } from "react-icons/io";
-import { FiSettings, FiHome, FiBarChart2 } from "react-icons/fi";
+import { FiSettings, FiHome } from "react-icons/fi";
 import { BsRecordCircle } from "react-icons/bs";
 import { HiOutlineDocumentReport } from "react-icons/hi";
+import { IoBusinessOutline } from "react-icons/io5";
+import { FaUsers } from "react-icons/fa";
+import { FaTableList } from "react-icons/fa6";
+import { FaRegCirclePause } from "react-icons/fa6";
+import { MdEmail, MdOutlineCampaign } from "react-icons/md";
+import { MdOutlineBackup } from "react-icons/md";
+import { TbBuildingCog } from "react-icons/tb";
+import { MdAccessAlarms } from "react-icons/md";
+import { MdOutlineMonitor } from "react-icons/md";
+import { TbMail } from "react-icons/tb";
+import { TiTicket } from "react-icons/ti";
+import { VscEditSession } from "react-icons/vsc";
+import { GrTemplate } from "react-icons/gr";
+import { TbMessageReport } from "react-icons/tb";
+import { FaHistory } from "react-icons/fa";
+import { BsReverseLayoutTextSidebarReverse } from "react-icons/bs";
+import { LuImport } from "react-icons/lu";
+import { PiStrategy } from "react-icons/pi";
+import { AiOutlineSchedule } from "react-icons/ai";
+import { MdOutlineIntegrationInstructions } from "react-icons/md";
+import { LuMonitorDot } from "react-icons/lu";
+import { RiBillLine } from "react-icons/ri";
 
 const SideBar = () => {
   const pathname = usePathname();
   const router = useRouter();
 
-  const activeMenuItems = [
-    { label: "Dashboard", route: "/dashboard", icon: <FiHome /> },
-    { label: "Gravações", route: "/recordings", icon: <BsRecordCircle /> },
-    {
-      label: "Relatórios",
-      route: "/reports",
-      icon: <HiOutlineDocumentReport />,
-    },
-  ];
-
   const menuItems = [
     //dashboard
     { label: "Configuração", route: "/sysconfiguration", icon: <FiHome /> },
     { label: "Gravações", route: "/recordings", icon: <BsRecordCircle /> },
-    {
-      label: "Relatórios",
-      route: "/reports",
-      icon: <HiOutlineDocumentReport />,
-    },
-    { label: "Monitoramento", route: "/monitor" },
-    { label: "Mailing & Estratégia", route: "/mailing" },
-    { label: "Bilhetagem", route: "/ticketing" },
-    { label: "Alarme", route: "/alarm" },
-    { label: "Sessões", route: "/session" },
-    { label: "Email", route: "/email" },
+    { label: "Relatórios", route: "/reports", icon: <HiOutlineDocumentReport />},
+    { label: "Monitoramento", route: "/monitor", icon: <MdOutlineMonitor /> },
+    { label: "Mailing & Estratégia", route: "/mailing", icon: <TbMail /> },
+    { label: "Bilhetagem", route: "/ticketing", icon: <TiTicket /> },
+    { label: "Alarme", route: "/alarm", icon: <MdAccessAlarms /> },
+    { label: "Sessões", route: "/session", icon: <VscEditSession /> },
+    { label: "Email", route: "/email", icon: <MdEmail /> },
   ];
 
   const menuItems2 = [
@@ -46,14 +54,22 @@ const SideBar = () => {
       route: "/sysconfiguration/generalConfig",
       icon: <FiSettings />,
     },
-    { label: "Organizações", route: "/sysconfiguration/customers" },
-    { label: "Usuários", route: "/users" },
-    { label: "Tabulações", route: "/tabulations" },
-    { label: "Pausas", route: "/pauses" },
-    { label: "Campanhas", route: "/campaigns" },
-    { label: "Backup", route: "/backup" },
-    { label: "Infraestrutura", route: "/infrastructure" },
-    { label: "Alarmes", route: "/alarms" },
+    {
+      label: "Organizações",
+      route: "/sysconfiguration/customers",
+      icon: <IoBusinessOutline />,
+    },
+    { label: "Usuários", route: "/users", icon: <FaUsers /> },
+    { label: "Tabulações", route: "/tabulations", icon: <FaTableList /> },
+    { label: "Pausas", route: "/pauses", icon: <FaRegCirclePause /> },
+    { label: "Campanhas", route: "/campaigns", icon: <MdOutlineCampaign /> },
+    { label: "Backup", route: "/backup", icon: <MdOutlineBackup /> },
+    {
+      label: "Infraestrutura",
+      route: "/infrastructure",
+      icon: <TbBuildingCog />,
+    },
+    { label: "Alarmes", route: "/alarms", icon: <MdAccessAlarms /> },
   ];
 
   const menuItems3 = [
@@ -69,33 +85,53 @@ const SideBar = () => {
       route: "/reports",
       icon: <HiOutlineDocumentReport />,
     },
-    { label: "Templates", route: "/templates" },
-    { label: "Relatórios Customizados", route: "/customReports" },
+    { label: "Templates", route: "/templates", icon: <GrTemplate /> },
+    {
+      label: "Relatórios Customizados",
+      route: "/customReports",
+      icon: <TbMessageReport />,
+    },
   ];
 
   const menuItems5 = [
     //mailing & estrategia
     { label: "Página Inicial", route: "/dashboard", icon: <FiHome /> },
-    { label: "Layout", route: "/layout" },
-    { label: "Importação", route: "/import" },
-    { label: "Estratégia", route: "/strategy" },
-    { label: "Mailing", route: "/mailing" },
-    { label: "Agendamento", route: "/scheduling" },
-    { label: "Integração", route: "/integration" },
+    {
+      label: "Layout",
+      route: "/layout",
+      icon: <BsReverseLayoutTextSidebarReverse />,
+    },
+    { label: "Importação", route: "/import", icon: <LuImport /> },
+    { label: "Estratégia", route: "/strategy", icon: <PiStrategy /> },
+    { label: "Mailing", route: "/mailing", icon: <TbMail /> },
+    { label: "Agendamento", route: "/scheduling", icon: <AiOutlineSchedule /> },
+    {
+      label: "Integração",
+      route: "/integration",
+      icon: <MdOutlineIntegrationInstructions />,
+    },
   ];
 
   const menuItems6 = [
     //bilhetagem
     { label: "Página Inicial", route: "/dashboard", icon: <FiHome /> },
-    { label: "Planos de Tarifacão", route: "/tariffPlans" },
-    { label: "Planos de Monitoração", route: "/monitoringPlans" },
-    { label: "Monitoração", route: "/monitoring" },
+    {
+      label: "Planos de Tarifacão",
+      route: "/tariffPlans",
+      icon: <RiBillLine />,
+    },
+    {
+      label: "Planos de Monitoração",
+      route: "/monitoringPlans",
+      icon: <LuMonitorDot />,
+    },
+    { label: "Monitoração", route: "/monitoring", icon: <MdOutlineMonitor /> },
     {
       label: "Relatórios",
       route: "/billingReports",
       icon: <HiOutlineDocumentReport />,
     },
-    { label: "Configurações", route: "/billingConfig" },
+    { label: "Configurações", route: "/billingConfig", icon: <FiSettings /> },
   ];
 
   //monitoramento(redireciona para uma pagina externa)
@@ -104,9 +140,13 @@ const SideBar = () => {
 
   const menuItems7 = [
     //sessao
-    { label: "Página Inicial", route: "/dashboard" },
-    { label: "Sessões Ativas", route: "/sessions" },
-    { label: "Histórico de Sessões", route: "/sessionHistory" },
+    { label: "Página Inicial", route: "/dashboard", icon: <FiHome /> },
+    { label: "Sessões Ativas", route: "/sessions", icon: <VscEditSession /> },
+    {
+      label: "Histórico de Sessões",
+      route: "/sessionHistory",
+      icon: <FaHistory />,
+    },
   ];
 
   const getActiveMenu = () => {
@@ -121,12 +161,13 @@ const SideBar = () => {
 
   const activeMenu = getActiveMenu();
 
+
   return (
     <>
       {/* Sidebar */}
       <aside className="group relative top-0 left-0 h-full bg-gradient-to-b from-[#87989e] via-[#0676ad] to-[#3CA9E6] text-white flex flex-col p-4 transition-all duration-300 w-20 hover:w-80 z-50">
         {/* Logo */}
-        <div className="mb-4 flex items-center space-x-2">
+        <div className="mb-6 ml-2 flex items-center space-x-2">
           <Image
             src="/Logo_olos.png"
             alt="Logo"
@@ -136,18 +177,10 @@ const SideBar = () => {
           />
         </div>
 
-        {/* Mensagem de boas-vindas */}
-        <div className="mb-8 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-sm leading-tight">
-          <p>
-            Você está acessando <strong>Professional Service</strong>.
-          </p>
-          <p>Boa tarde, seu último login foi</p>
-        </div>
-
         {/* Menu com ícones e labels no hover */}
-        <nav className="mb-6 block group-hover:hidden">
+        <nav className="mb-6 block group-hover:hidden ml-3">
           <ul className="flex flex-col gap-4">
-            {activeMenuItems.map((item) => (
+            {activeMenu.map((item) => (
               <li
                 key={item.label}
                 className="flex items-center gap-3 cursor-pointer hover:text-[#EC7D22] transition-all duration-300 hover:scale-105"
@@ -161,6 +194,14 @@ const SideBar = () => {
             ))}
           </ul>
         </nav>
+
+        {/* Mensagem de boas-vindas */}
+        <div className="mb-8 ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-sm leading-tight">
+          <p>
+            Você está acessando <strong>Professional Service</strong>.
+          </p>
+          <p>Boa tarde, seu último login foi</p>
+        </div>
 
         {/* Submenu dinâmico com base na rota */}
         <nav className="hidden group-hover:block opacity-0 group-hover:opacity-100 transition-opacity duration-300 pl-1">
